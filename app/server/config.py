21228714d9270905f5b7b477b445c24a603c51d1
mahdi kiani <window1992@live.com>
2024-11-26 00:22:45 +0300

    Initial commit

---
+++ app/server/config.py
@@ -0,0 +1,62 @@
+"""FastAPI server configuration."""
+
+import dataclasses
+import logging
+import logging.config
+import os
+from pathlib import Path
+
+import dotenv
+from singleton import Singleton
+
+dotenv.load_dotenv()
+
+
+@dataclasses.dataclass
+class Settings(metaclass=Singleton):
+    """Server config settings."""
+
+    root_url: str = os.getenv("DOMAIN", default="http://localhost:8000")
+    base_dir: Path = Path(__file__).resolve().parent.parent
+
+    testing: bool = os.getenv("TESTING", default=False)
+
+    log_config = {
+        "version": 1,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "standard",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "level": "INFO",
+                "filename": base_dir / "logs" / "info.log",
+                "formatter": "standard",
+            },
+        },
+        "formatters": {
+            "standard": {
+                "format": "[{levelname} : {filename}:{lineno} : {asctime} -> {funcName:10}] {message}",
+                # "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+                "style": "{",
+            }
+        },
+        "loggers": {
+            "": {
+                "handlers": [
+                    "console",
+                    "file",
+                ],
+                "level": "INFO",
+                "propagate": True,
+            }
+        },
+    }
+
+    def config_logger(cls):
+        if not (cls.base_dir / "logs").exists():
+            (cls.base_dir / "logs").mkdir()
+
+        logging.config.dictConfig(cls.log_config)
