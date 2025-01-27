1230b731f01c0b29067d2c6474de55d301075782
Mahdi Kiani <mahdikiany@gmail.com>
2025-01-27 17:25:03 +0000

    add perplexity new models

--- app/apps/ai/engines.py
+++ app/apps/ai/engines.py
@@ -33,6 +33,8 @@ class Perplexity(AIEngine):
     @property
     def model(self):
         return [
+            "sonar-pro",
+            "sonar",
             "llama-3.1-sonar-huge-128k-online",
             "llama-3.1-sonar-large-128k-online",
             "llama-3.1-sonar-small-128k-online",
