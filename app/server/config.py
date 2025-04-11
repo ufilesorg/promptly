23ada2c483e8ae7b21ecd4fd84a1c4b5b5245565
Mahdi Kiani <mahdikiany@gmail.com>
2025-04-11 13:08:13 +0000

    message.uln.me

--- app/server/config.py
+++ app/server/config.py
@@ -15,7 +15,5 @@ class Settings(config.Settings):
     base_dir: Path = Path(__file__).resolve().parent.parent
     base_path: str = "/v1/apps/promptly"
 
-    STRAPI_URL: str = os.getenv(
-        "STRAPI_URL", "https://message.bot.inbeet.tech/api/prompts"
-    )
+    STRAPI_URL: str = os.getenv("STRAPI_URL", "https://message.uln.me/api/prompts")
     STRAPI_TOKEN: str = os.getenv("STRAPI_TOKEN")
